480eab4998a85a7ca8521d23de67f57f600e3d12
7Bpencil <efagot32@gmail.com>
2020-04-02 21:14:18 +0500

    Last version

--- App/Playground.cs
+++ App/Playground.cs
@@ -11,22 +11,32 @@ namespace App
         private readonly HashSet<Keys> pressedKeys = new HashSet<Keys>();
         private static Keys keyPressed;
         private Point positionPlayer;
+        private Point positionPlayerCenter;
+
+        private Point playerWidthHeight;
         private Point positionMouse;
 
         public Playground()
         {
-            setUpForm();
-            positionPlayer = new Point(ClientSize.Width / 2, ClientSize.Height / 2);
+            SetUpForm();
+            SetUpPlayer();
             var timer = new Timer();
             timer.Interval = 15;
             timer.Tick += MainLoop;
             timer.Start();
         }
 
-        private void setUpForm() 
+        private void SetUpForm()
         {
+            DoubleBuffered = true;
             Size = new Size(854, 480);
             Text = "NEW GAME";
+        }
+
+        private void SetUpPlayer()
+        {
+            playerWidthHeight = new Point(50, 50);
+            positionPlayer = new Point(ClientSize.Width / 2, ClientSize.Height / 2);
         }
         
         private void MainLoop(object sender, EventArgs args)
@@ -38,7 +48,14 @@ namespace App
         protected override void OnPaint(PaintEventArgs e)
         {
             var g = e.Graphics;
-            g.FillRectangle(Brushes.Crimson, positionPlayer.X, positionPlayer.Y, 50, 50);
+            
+            g.FillRectangle(Brushes.Crimson, -100, -100, 200, 200);
+            g.FillRectangle(Brushes.Crimson, 300, 300, 50, 50);
+            g.FillRectangle(Brushes.Crimson, 100, 300, 50, 50);
+            g.FillRectangle(Brushes.Crimson, 300, 100, 1000, 1000);
+            
+            g.FillRectangle(Brushes.Cyan, positionPlayer.X, positionPlayer.Y, playerWidthHeight.X, playerWidthHeight.Y);
+            g.FillEllipse(Brushes.Blue, positionPlayerCenter.X - 5, positionPlayerCenter.Y - 5, 10, 10);
             g.FillEllipse(Brushes.Black, positionMouse.X, positionMouse.Y, 10, 10);
         }
 
@@ -59,12 +76,13 @@ namespace App
             var deltaX = 0;
             var deltaY = 0;
 
-            if (keyPressed == Keys.Down) deltaY = 2;
-            else if (keyPressed == Keys.Left) deltaX = -2;
-            else if (keyPressed == Keys.Up) deltaY = -2;
-            else if (keyPressed == Keys.Right) deltaX = 2;
+            if (keyPressed == Keys.Down) deltaY = 4;
+            else if (keyPressed == Keys.Left) deltaX = -4;
+            else if (keyPressed == Keys.Up) deltaY = -4;
+            else if (keyPressed == Keys.Right) deltaX = 4;
             
             positionPlayer = new Point(positionPlayer.X + deltaX, positionPlayer.Y + deltaY);
+            positionPlayerCenter = new Point(positionPlayer.X + playerWidthHeight.X / 2,positionPlayer.Y + playerWidthHeight.Y / 2);
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
