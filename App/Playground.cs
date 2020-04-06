08be311c13257725b95c801768bfd72a50b854e9
Vladislav Zaripov <mr.vladislavzaripov@mail.ru>
2020-04-06 19:55:30 +0500

    Load simple level scene as image

--- App/Playground.cs
+++ App/Playground.cs
@@ -4,72 +4,75 @@ using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace App
+namespace App.View
 {
-    public class Playground : Form
+
+    public partial class PlaygroundDeprecated : Form
     {
-        private readonly HashSet<Keys> pressedKeys = new HashSet<Keys>();
-        private static Keys keyPressed;
-        private Point positionPlayer;
-        private Point positionMouse;
+        private System.ComponentModel.IContainer components = null;
+        private PictureBox pb;
+        private Bitmap surface = null;
+        private Graphics device;
+        private Bitmap image = null;
+        private PictureBox pb1;
 
-        public Playground()
+        public PlaygroundDeprecated()
         {
-            setUpForm();
-            positionPlayer = new Point(ClientSize.Width / 2, ClientSize.Height / 2);
-            var timer = new Timer();
-            timer.Interval = 15;
-            timer.Tick += MainLoop;
-            timer.Start();
-        }
-
-        private void setUpForm() 
-        {
-            Size = new Size(854, 480);
-            Text = "NEW GAME";
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pb1.Parent = this;//it is really need for drawing and create a scene
+            this.pb1.BackColor = System.Drawing.Color.Black;
+            this.pb1.Location = new System.Drawing.Point(0, 0);
+            this.pb1.Name = "box";
+            this.pb1.Size = new System.Drawing.Size(865, 706);
+            this.pb1.TabStop = false; 
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Game_FormClosed);
         }
         
-        private void MainLoop(object sender, EventArgs args)
+        protected override void Dispose(bool disposing)
         {
-            Move();
-            Invalidate();
-        }
-        
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            var g = e.Graphics;
-            g.FillRectangle(Brushes.Crimson, positionPlayer.X, positionPlayer.Y, 50, 50);
-            g.FillEllipse(Brushes.Black, positionMouse.X, positionMouse.Y, 10, 10);
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        protected override void OnKeyDown(KeyEventArgs e)
+        private void Game_Load(object sender, EventArgs e)
         {
-            pressedKeys.Add(e.KeyCode);
-            keyPressed = e.KeyCode;
-        }
+            //set up the form
+            this.Text = "Text drawing Demo";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Size = new Size(865, 706);//this is size of image
 
-        protected override void OnKeyUp(KeyEventArgs e)
-        {
-            pressedKeys.Remove(e.KeyCode);
-            keyPressed = pressedKeys.Any() ? pressedKeys.Min() : Keys.None;
-        }
-        
-        public void Move()
-        {
-            var deltaX = 0;
-            var deltaY = 0;
-
-            if (keyPressed == Keys.Down) deltaY = 2;
-            else if (keyPressed == Keys.Left) deltaX = -2;
-            else if (keyPressed == Keys.Up) deltaY = -2;
-            else if (keyPressed == Keys.Right) deltaX = 2;
+            surface = new Bitmap(this.Size.Width, this.Size.Height);
+            pb1.Image = surface;
+            device = Graphics.FromImage(surface);
             
-            positionPlayer = new Point(positionPlayer.X + deltaX, positionPlayer.Y + deltaY);
+            var path = System.IO.Path.GetFullPath(@"img/grass.bmp");
+            image = LoadBitmap(path);
+            device.DrawImage(image, 0, 0);
         }
 
-        protected override void OnMouseMove(MouseEventArgs e)
+        public Bitmap LoadBitmap(string filename)
         {
-            positionMouse = new Point(e.X, e.Y);
+            Bitmap bmp = null;
+            try
+            {
+                bmp = new Bitmap(filename);
+            }
+            catch (Exception ex) { }
+            return bmp;
+        }
+        
+        
+        private void Game_FormClosed(object sender, EventArgs e)
+        {
+            device.Dispose();
+            surface.Dispose();
         }
     }
 }
