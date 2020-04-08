428dc3ca1983cdc7946eff25f395dbb188b707a7
7Bpencil <efagot32@gmail.com>
2020-04-08 10:37:26 +0500

    Real fancy juicy slassy controls improvement! Now we can check combinations of pressed keys!

--- App/Engine/Core.cs
+++ App/Engine/Core.cs
@@ -13,13 +13,17 @@ namespace App.Engine
     public class Core : ContractCore
     {
         private ContractView view;
-        private HashSet<Keys> pressedKeys;
-        private Keys keyPressed;
+        private keyStates keyState;
         private List<RigidShape> sceneObjects;
         private CollisionDetection collisionDetection;
         private List<CollisionInfo> collisions;
         private RigidShape player;
         private RigidShape cursor;
+        
+        class keyStates
+        {
+            public bool up, down, left, right;
+        }
 
         public Core(ContractView view)
         {
@@ -27,7 +31,7 @@ namespace App.Engine
             var objectManager = new ObjectFactory();
             collisionDetection = new CollisionDetection();
             sceneObjects = objectManager.GetSceneObjects(out player, out cursor, view.ClientSize.Width, view.ClientSize.Height);
-            pressedKeys = new HashSet<Keys>();
+            keyState = new keyStates();
         }
 
         public override void GameLoop(object sender, EventArgs args)
@@ -49,10 +53,10 @@ namespace App.Engine
             var deltaX = 0;
             var deltaY = 0;
 
-            if (keyPressed == Keys.Down || keyPressed == Keys.S) deltaY = 4;
-            else if (keyPressed == Keys.Left || keyPressed == Keys.A) deltaX = -4;
-            else if (keyPressed == Keys.Up || keyPressed == Keys.W) deltaY = -4;
-            else if (keyPressed == Keys.Right || keyPressed == Keys.D) deltaX = 4;
+            if (keyState.right) deltaX += 4;
+            if (keyState.up) deltaY -= 4;
+            if (keyState.left) deltaX -= 4;
+            if (keyState.down) deltaY += 4;
 
             player.Move(new Vector(deltaX, deltaY));
         }
@@ -61,19 +65,63 @@ namespace App.Engine
         {
             cursor.Center = newPosition;
         }
-
+        
         public override void OnKeyDown(Keys keyPressed)
         {
-            pressedKeys.Add(keyPressed);
-            this.keyPressed = keyPressed;
+            switch (keyPressed)
+            {
+                case Keys.Up:
+                case Keys.W:
+                    keyState.up = true;
+                    break;
+
+                case Keys.Down:
+                case Keys.S:
+                    keyState.down = true;
+                    break;
+
+                case Keys.Left:
+                case Keys.A:
+                    keyState.left = true;
+                    break;
+
+                case Keys.Right:
+                case Keys.D:
+                    keyState.right = true;
+                    break;
+            }
         }
 
         public override void OnKeyUp(Keys keyPressed)
         {
-            pressedKeys.Remove(keyPressed);
-            this.keyPressed = pressedKeys.Any() ? pressedKeys.Min() : Keys.None;
+            switch (keyPressed)
+            {
+                case Keys.Escape:
+                    Application.Exit();
+                    break;
+
+                case Keys.Up:
+                case Keys.W:
+                    keyState.up = false;
+                    break;
+
+                case Keys.Down:
+                case Keys.S:
+                    keyState.down = false;
+                    break;
+
+                case Keys.Left:
+                case Keys.A:
+                    keyState.left = false;
+                    break;
+
+                case Keys.Right:
+                case Keys.D:
+                    keyState.right = false;
+                    break;
+            }
         }
-        
+
         public override List<RigidShape> GetSceneObjects()
         {
             return sceneObjects;
