a44ea3cf2b2f7b8ff2a18d9cc5ca815c8a1c5fa8
Vladislav Zaripov <mr.vladislavzaripov@mail.ru>
2020-04-08 12:43:48 +0500

    work with buffer

--- App/View/PlaygroundDeprecated.cs
+++ App/View/PlaygroundDeprecated.cs
@@ -13,13 +13,6 @@ namespace App.View
     {
         private System.ComponentModel.IContainer components = null;
 
-        public struct tilemapStruct
-        {
-            public int tilenum;
-            public string data1;
-            public bool collidable;
-        }
-
         public struct keyStates
         {
             public bool up, down, left, right;
@@ -29,17 +22,17 @@ namespace App.View
         const int COLUMNS = 5; //Why only 5?
 
         //only for surface
-        private Bitmap bmpTiles;
         private Bitmap bmpSurface;
         private PictureBox pbSurface;
         private Graphics gfxSurface;
         private Font fontArial;
-        private tilemapStruct[] tilemap;
         private PointF scrollPos = new PointF(0, 0);
         private PointF oldScrollPos = new PointF(-1, -1);
         private keyStates keyState;
         private Timer timer1;
 
+        private Bitmap bmpScrollBuffer;
+        private Graphics gfxScrollBuffer;
         public PlaygroundDeprecated()
         {
             this.ClientSize = new System.Drawing.Size(284, 262);
@@ -62,11 +55,8 @@ namespace App.View
 
         private void PlaygroundDeprecated_Load(object sender, EventArgs e)
         {
-            this.Text = "Smooth Scroller";
+            this.Text = "Sub-Tile Buffer Demo";
             this.Size = new Size(800 + 16, 600 + 38);
-
-            //create tilemap
-            tilemap = new tilemapStruct[128 * 128];
 
             //set up level drawing surface
             bmpSurface = new Bitmap(800, 600);
@@ -76,15 +66,29 @@ namespace App.View
             pbSurface.Dock = DockStyle.Fill;
             pbSurface.Image = bmpSurface;
             gfxSurface = Graphics.FromImage(bmpSurface);
+            
+            //create scroll buffer
+            bmpScrollBuffer = new Bitmap(25 * 32 + 64, 19 * 32 + 64);
+            gfxScrollBuffer = Graphics.FromImage(bmpScrollBuffer);
 
+            //fill buffer border area
+            gfxScrollBuffer.FillRectangle(Brushes.Gray,
+                new Rectangle(0, 0, bmpScrollBuffer.Width, bmpScrollBuffer.Height));
+            
+            //fill buffer screen area
+            gfxScrollBuffer.FillRectangle(Brushes.BlueViolet,
+                new Rectangle(32, 32, 25 * 32, 19 * 32));
+            
+            
+            //it need to show how it works
+            for (int y = 0; y < 19; y++)
+                for (int x = 0; x < 25; x++)
+                    gfxScrollBuffer.DrawRectangle(Pens.White, 32 + x * 32, 32 + y * 32, 32, 32);
+                
             //create font
             fontArial = new Font("Arial Narrow", 18);
-
-            //load tiles bitmap
-            bmpTiles = new Bitmap("Images/palette.bmp");
             
-            //load the tilemap
-            loadTilemapFile("Levels/level1.level");
+            gfxScrollBuffer.DrawString("SCROLL BUFFER BORDER", fontArial, Brushes.White, 0, 0);
 
             //create the timer
             timer1 = new Timer();
@@ -95,122 +99,47 @@ namespace App.View
 
         private void timer1_tick(object sender, EventArgs e)
         {
-            if (keyState.up)
-            {
-                scrollPos.Y -= 1;
-                if (scrollPos.Y < 0) scrollPos.Y = 0;
-            }
             if (keyState.down)
             {
-                scrollPos.Y += 1;
-                if (scrollPos.Y > 127 - 19) scrollPos.Y = 127 - 19;
+                scrollPos.Y -= 4;
+                if (scrollPos.Y < -64) scrollPos.Y = -64;
             }
-            if (keyState.left)
+            if (keyState.up)
             {
-                scrollPos.X -= 1;
-                if (scrollPos.X < 0) scrollPos.X = 0;
+                scrollPos.Y += 4;
+                if (scrollPos.Y > 0) scrollPos.Y = 0;
             }
             if (keyState.right)
             {
-                scrollPos.X += 1;
-                if (scrollPos.X > 127 - 25) scrollPos.X = 127 - 25;
+                scrollPos.X -= 4;
+                if (scrollPos.X < -64) scrollPos.X = -64;
+            }
+            if (keyState.left)
+            {
+                scrollPos.X += 4;
+                if (scrollPos.X > 0) scrollPos.X = 0;
             }
             
-            drawTilemap();
-            string text = "Scroll " + scrollPos.ToString();
-            gfxSurface.DrawString(text, fontArial, Brushes.White, 10, 10);
-        }
-
-        private void loadTilemapFile(string filename)
-        {
-            try
-            {
-                XmlDocument doc = new XmlDocument();
-                doc.Load(filename);
-                XmlNodeList nodelist = doc.GetElementsByTagName("tiles");
-                foreach (var node in nodelist)
-                {
-                    XmlElement element = (XmlElement) node;
-                    int index = 0;
-                    int value = 0;
-                    string data1 = "";
-                    bool collidable = false;
-                    
-                    //read tile index #
-                    index = Convert.ToInt32(element.GetElementsByTagName("tile")[0].InnerText);
-                    
-                    //read tilenum
-                    value = Convert.ToInt32(element.GetElementsByTagName(
-                        "value")[0].InnerText);
-                    
-                    //read data1
-                    data1 = Convert.ToString(element.GetElementsByTagName(
-                        "data1")[0].InnerText);
-                    
-                    //read collidable
-                    collidable = Convert.ToBoolean(element.GetElementsByTagName(
-                        "collidable")[0].InnerText);
-
-                    tilemap[index].tilenum = value;
-                    tilemap[index].data1 = data1;
-                    tilemap[index].collidable = collidable;
-                }
-            }
-            catch (Exception es)
-            {
-                MessageBox.Show(es.Message);
-            }
-        }
-
-        private void drawTilemap()
-        {
-            int tilenum, sx, sy;
-            for (int x = 0; x < 25; ++x)
-            for (int y = 0; y < 19; ++y)
-            {
-                sx = (int) scrollPos.X + x;
-                sy = (int) scrollPos.Y + y;
-                tilenum = tilemap[sy * 128 + sx].tilenum;
-                drawTileNumber(x, y, tilenum);
-            }
-            string text = "Scroll " + scrollPos.ToString();
-            gfxSurface.DrawString(text, fontArial, Brushes.White, 10, 10);
-        }
-
-        public void drawTileNumber(int x, int y, int tile)
-        {
-            //draw tile
-            int sx = (tile % COLUMNS) * 33;//columns it mean columns with tiles in palette
-            int sy = (tile / COLUMNS) * 33;//
-            Rectangle src = new Rectangle(sx, sy, 32, 32);
-            int dx = x * 32;
-            int dy = y * 32;
-            gfxSurface.DrawImage(bmpTiles, dx, dy, src, GraphicsUnit.Pixel);
+            //draw scroll buffer
+            gfxSurface.DrawImage(bmpScrollBuffer, scrollPos);
             
-            //save changes
+            //print scroll position
+            string text = "Scroll " + scrollPos.ToString();
+            gfxSurface.DrawString(text, fontArial, Brushes.White, 650, 0);
+
             pbSurface.Image = bmpSurface;
         }
         
-        public Bitmap LoadBitmap(string filename)
-        {
-            Bitmap bmp = null;
-            try
-            {
-                bmp = new Bitmap(filename);
-            }
-            catch (Exception ex)
-            {
-            }
-
-            return bmp;
-        }
-
 
         private void Playground_FormClosed(object sender, EventArgs e)
         {
             bmpSurface.Dispose();
             pbSurface.Dispose();
             gfxSurface.Dispose();
+            bmpScrollBuffer.Dispose();
+            gfxScrollBuffer.Dispose();
+            fontArial.Dispose();
+            timer1.Dispose();
         }
 
         private void PlaygroundDeprecated_KeyUp(object sender, KeyEventArgs e)
