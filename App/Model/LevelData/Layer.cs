2f22a161b376812c840a0591e3d9e20c0f2de38d
7Bpencil <efagot32@gmail.com>
2020-04-09 12:46:31 +0500

    some cleaning in level parser and level data

--- App/Model/LevelData/Layer.cs
+++ App/Model/LevelData/Layer.cs
@@ -7,18 +7,5 @@
         public int Width;
         public int Height;
         public int[] Tiles;
-
-        public Layer(int id, string name, int width, int height)
-        {
-            Id = id;
-            Name = name;
-            Width = width;
-            Height = height;
-            Tiles = new int[width * height];
-        }
-
-        public Layer()
-        {
-        }
     }
 }
