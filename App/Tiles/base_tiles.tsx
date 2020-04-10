e3c1818178c026542766ac92d16d169ad46c552b
7Bpencil <efagot32@gmail.com>
2020-04-10 14:28:07 +0500

    dfwef

---
+++ App/Tiles/base_tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="base_tiles" tilewidth="64" tileheight="64" tilecount="144" columns="9">
+ <image source="sprite_map.bmp" trans="000000" width="576" height="1024"/>
+ <tile id="131">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0" y="49" width="64" height="15"/>
+  </objectgroup>
+ </tile>
+</tileset>
