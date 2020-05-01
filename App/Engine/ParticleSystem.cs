9acd890d233d4f01740782707eaba5f15ed6eb54
7Bpencil <efagot32@gmail.com>
2020-05-01 17:06:05 +0500

    new structure - ParticleContainer and ParticleUnit

--- App/Engine/ParticleSystem.cs
+++ App/Engine/ParticleSystem.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using App.Engine.Physics;
+
 namespace App.Engine
 {
     public class ParticleSystem
@@ -6,6 +9,29 @@ namespace App.Engine
 
     public class ParticleContainer
     {
-        
+        private Sprite particleSprite;
+        private readonly List<ParticleUnit> particleUnits;
+
+        public ParticleContainer(Sprite particleSprite, int startCapacity)
+        {
+            this.particleSprite = particleSprite;
+            particleUnits = new List<ParticleUnit> {Capacity = startCapacity};
+        }
+
+        public void AddUnit(ParticleUnit newUnit) => particleUnits.Add(newUnit);
+    }
+
+    public struct ParticleUnit
+    {
+        public readonly Vector particlePosition;
+        public readonly float particleAngle;
+        public bool IsExpired;
+
+        public ParticleUnit(Vector particlePosition, float particleAngle)
+        {
+            this.particlePosition = particlePosition;
+            this.particleAngle = particleAngle;
+            IsExpired = false;
+        }
     }
 }
