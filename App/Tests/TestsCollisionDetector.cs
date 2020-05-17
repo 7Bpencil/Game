d619dc2e312508dfb856ac6fd54132f52ec7fd78
7Bpencil <efagot32@gmail.com>
2020-05-17 14:22:23 +0500

    first test

--- App/Tests/TestsCollisionDetector.cs
+++ App/Tests/TestsCollisionDetector.cs
@@ -1,7 +1,19 @@
+using App.Engine.Physics;
+using App.Engine.Physics.Collision;
+using App.Engine.Physics.RigidShapes;
+using NUnit.Framework;
+
 namespace App.Tests
 {
+    [TestFixture]
     public class TestsCollisionDetector
     {
-        
+        [Test]
+        public void TestSimpleCirclesCollision()
+        {
+            var first = new RigidCircle(Vector.ZeroVector, 10, true, true);
+            var second = new RigidCircle(new Vector(0, 15), 10, true, true);
+            Assert.IsNotNull(CollisionDetector.GetCollisionInfo(first, second));
+        }
     }
 }
